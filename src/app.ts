e6ef58f8294377bf5ee097cdb0a92f690b24e48f
Matheus Ryuki <dr_4_gao@hotmail.com>
2020-05-04 14:23:17 -0300

    FInalizado projeto

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { GOOGLE_KEY } from "../env";
 import axios from "axios";
 
+declare var google: any;
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
@@ -17,13 +19,21 @@ const searchAddressHandler = (event: Event) => {
 
   axios
     .get<GoogleGeocodingResponse>(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_KEY}`
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_KEY}`
     )
     .then((response) => {
-      if (response.data.status !== 'OK') {
-        throw new Error("NÃO CONSEGUIU ACHAR UM LUGAR")
+      if (response.data.status !== "OK") {
+        throw new Error("NÃO CONSEGUIU ACHAR UM LUGAR");
       }
       const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coordinates,
+        zoom: 16,
+      });
+
+      new google.maps.Marker({ position: coordinates, map: map });
     })
     .catch((err) => {
       alert(err);
