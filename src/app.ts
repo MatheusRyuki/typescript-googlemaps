973f6eec003c386a1a308667ca3b32a281dfd687
Matheus Ryuki <dr_4_gao@hotmail.com>
2020-05-04 14:07:29 -0300

    usando axios para carregar o mapa do endereço

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,33 @@
 import { GOOGLE_KEY } from "../env";
+import axios from "axios";
 
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
+type GoogleGeocodingResponse = {
+  results: {
+    geometry: { location: { lat: number; lng: number } };
+  }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
 const searchAddressHandler = (event: Event) => {
   event.preventDefault();
-  const enteredAddress = addressInput.value; 
+  const enteredAddress = addressInput.value;
 
-
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== 'OK') {
+        throw new Error("NÃO CONSEGUIU ACHAR UM LUGAR")
+      }
+      const coordinates = response.data.results[0].geometry.location;
+    })
+    .catch((err) => {
+      alert(err);
+    });
 };
 
 form.addEventListener("submit", searchAddressHandler);
